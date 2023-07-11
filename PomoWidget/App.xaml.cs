3f0d4fa074dea096e7839a9eda59b734e5da5d75
Antony Carrard <antony.carrard@heig-vd.ch>
2023-07-11 22:54:47 +0200

    Removed unused class

--- PomoWidget/App.xaml.cs
+++ PomoWidget/App.xaml.cs
@@ -1,17 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace PomoWidget
+namespace PomoWidget;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly TimeManager _timeManager;
+    private readonly NavigationStore _navigationStore;
+
+    public App()
     {
+        _timeManager = new TimeManager(
+            new TimeSpan(0, 25, 0),
+            new TimeSpan(0, 5, 0),
+            new TimeSpan(0, 15, 0),
+            4);   // TODO : maybe apply the parameters in the OnStartup method, to get the default parameters from the settings view.
+			_navigationStore = new NavigationStore();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _navigationStore.CurrentViewModel = CreateHomeViewModel();
+
+        MainWindow = new MainWindow()
+        {
+            DataContext = new MainViewModel(_navigationStore)
+        };
+        MainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    private SettingsViewModel CreateSettingsViewModel()
+    {
+        return new SettingsViewModel(_timeManager, new NavigationService(_navigationStore, CreateHomeViewModel));
+    }
+
+    private HomeViewModel CreateHomeViewModel()
+    {
+        return new HomeViewModel(_timeManager, new NavigationService(_navigationStore, CreateSettingsViewModel));
     }
 }
--- PomoWidget/MainWindow.xaml.cs
+++ PomoWidget/MainWindow.xaml.cs
@@ -1,158 +1,154 @@
-﻿using Microsoft.Win32;
-using PomoWidget.Timer;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Documents.DocumentStructures;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
+﻿using System.Windows;
 
-namespace PomoWidget
+namespace PomoWidget;
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+
+    //DispatcherTimer dt = new DispatcherTimer();
+    //TimeManager timeManager = new TimeManager(25, 5, 15, 4);
+    //TimeSpan ts = TimeSpan.Zero;
+    //private string _CurrentTime = string.Empty;
+    //public string CurrentTime
+    //{
+    //    get { return _CurrentTime; }
+    //    set
+    //    {
+    //        _CurrentTime = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+    //#region INotifyPropertyChanged Members
+
+    //public event PropertyChangedEventHandler? PropertyChanged;
+
+    ///// <summary>
+    ///// Raises this object's PropertyChanged event.
+    ///// </summary>
+    ///// <param name="propertyName">The property that has a new value.</param>
+    //protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    //{
+    //    PropertyChangedEventHandler handler = this.PropertyChanged;
+    //    if (handler != null)
+    //    {
+    //        var e = new PropertyChangedEventArgs(propertyName);
+    //        handler(this, e);
+    //    }
+    //}
+    //#endregion
+
+    public MainWindow()
     {
-
-        DispatcherTimer dt = new DispatcherTimer();
-        TimeManager timeManager = new TimeManager();
-        TimeSpan ts = TimeSpan.Zero;
-        private string _CurrentTime = string.Empty;
-        public string CurrentTime
-        {
-            get { return _CurrentTime; }
-            set
-            {
-                _CurrentTime = value;
-                OnPropertyChanged();
-            }
-        }
-        #region INotifyPropertyChanged Members
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = this.PropertyChanged;
-            if (handler != null)
-            {
-                var e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            DataContext = this;
-            dt.Tick += new EventHandler(Dt_Tick);
-            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
-            PomodoroToggleButton.IsChecked = true;
-            ConvertTimeFormat();
-            timeManager.StateChange += StateChange; // register with an event
-        }
-
-        void ConvertTimeFormat()
-        {
-            ts = timeManager.StopWatch.Elapsed;
-            ts = timeManager.CurrentTimer - ts;
-            CurrentTime = string.Format("{0:00}:{1:00}",
-                ts.Minutes, ts.Seconds);
-        }
-
-        void Dt_Tick(object sender, EventArgs e)
-        {
-            if (timeManager.StopWatch.IsRunning)
-            {
-                ConvertTimeFormat();
-                if (ts.TotalSeconds <= 0)
-                {
-                    timeManager.RequestStateChange();
-                }
-            }
-        }
-
-        private void ResetButton_Click(object sender, RoutedEventArgs e)
-        {
-            timeManager.ResetPressed();
-        }
-
-        private void StartToggleButton_Checked(object sender, RoutedEventArgs e)
-        {
-            timeManager.StopWatch.Start();
-            dt.Start();
-            StartToggleButton.Content = "PAUSE";
-        }
-
-        private void StartToggleButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            if (timeManager.StopWatch.IsRunning)
-                timeManager.StopWatch.Stop();
-            StartToggleButton.Content = "START";
-        }
-
-        private void PomodoroToggleButton_Checked(object sender, RoutedEventArgs e)
-        {
-            timeManager.PomodoroPressed();
-        }
-
-        private void ShortBreakToggleButton_Checked(object sender, RoutedEventArgs e)
-        {
-            timeManager.ShortBreakPressed();
-        }
-
-        private void LongBreakToggleButton_Checked(object sender, RoutedEventArgs e)
-        {
-            timeManager.LongBreakPressed();
-        }
-
-        private void SkipButton_Click(object sender, RoutedEventArgs e)
-        {
-            timeManager.SkipPressed();
-        }
-
-        // event handler
-        public void StateChange(object sender, EventArgs e)
-        {
-            StartToggleButton.IsChecked = false;
-            ConvertTimeFormat();
-            switch(timeManager.TimeState)
-            {
-                case TimeState.Pomodoro:
-                    {
-                        PomodoroToggleButton.IsChecked = true;
-                        break;
-                    }
-                case TimeState.ShortBreak:
-                    {
-                        ShortBreakToggleButton.IsChecked = true;
-                        break;
-                    }
-                case TimeState.LongBreak:
-                    {
-                        LongBreakToggleButton.IsChecked = true;
-                        break;
-                    }
-            }
-        }
+        InitializeComponent();
+        //DataContext = new MainViewModel();
+        //dt.Tick += new EventHandler(Dt_Tick);
+        //dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        ////FocusToggleButton.IsChecked = true;
+        //ConvertTimeFormat();
+        //timeManager.StateChange += StateChange; // register with an event
     }
+
+    //void ConvertTimeFormat()
+    //{
+    //    ts = timeManager.StopWatch.Elapsed;
+    //    ts = timeManager.CurrentInterval - ts;
+    //    CurrentTime = string.Format("{0:00}:{1:00}",
+    //        ts.Minutes, ts.Seconds);
+    //}
+
+    //void Dt_Tick(object sender, EventArgs e)
+    //{
+    //    if (timeManager.StopWatch.IsRunning)
+    //    {
+    //        ConvertTimeFormat();
+    //        if (ts.TotalSeconds <= 0)
+    //        {
+    //            timeManager.RequestStateChange();
+    //        }
+    //    }
+    //}
+
+    //private void ResetButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    timeManager.ResetPressed();
+    //}
+
+    //private void StartToggleButton_Checked(object sender, RoutedEventArgs e)
+    //{
+    //    timeManager.StopWatch.Start();
+    //    dt.Start();
+    //    //StartToggleButton.Content = "PAUSE";
+    //}
+
+    //private void StartToggleButton_Unchecked(object sender, RoutedEventArgs e)
+    //{
+    //    if (timeManager.StopWatch.IsRunning)
+    //        timeManager.StopWatch.Stop();
+    //    //StartToggleButton.Content = "START";
+    //}
+
+    //private void FocusToggleButton_Checked(object sender, RoutedEventArgs e)
+    //{
+    //    timeManager.FocusPressed();
+    //}
+
+    //private void ShortBreakToggleButton_Checked(object sender, RoutedEventArgs e)
+    //{
+    //    timeManager.ShortBreakPressed();
+    //}
+
+    //private void LongBreakToggleButton_Checked(object sender, RoutedEventArgs e)
+    //{
+    //    timeManager.LongBreakPressed();
+    //}
+
+    //private void SkipButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    timeManager.SkipPressed();
+    //}
+
+    //// event handler
+    //public void StateChange(object sender, EventArgs e)
+    //{
+    //    //StartToggleButton.IsChecked = false;
+    //    ConvertTimeFormat();
+    //    switch(timeManager.TimeState)
+    //    {
+    //        case TimeState.Focus:
+    //            {
+    //                //FocusToggleButton.IsChecked = true;
+    //                break;
+    //            }
+    //        case TimeState.ShortBreak:
+    //            {
+    //                //ShortBreakToggleButton.IsChecked = true;
+    //                break;
+    //            }
+    //        case TimeState.LongBreak:
+    //            {
+    //                //LongBreakToggleButton.IsChecked = true;
+    //                break;
+    //            }
+    //    }
+    //}
+
+    //private void SettingsButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    Window window = new Window
+    //    {
+    //        Title = "My User Control Dialog",
+    //        Content = new Settings(),
+    //        Width = this.Width,
+    //        Height = this.Height,
+    //        Top = this.Top,
+    //        Left = this.Left,
+    //        Owner = this
+    //    };
+
+    //    this.Hide();
+    //    window.ShowDialog();
+    //}
 }
